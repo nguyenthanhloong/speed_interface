526c02455a86fc558b80843e48781e6e60e0bfe6
nguyenthanhloong <ngthanhlong.cntt@gmail.com>
2026-04-26 18:34:43 +0700

    REMOVE ENV DEVELOPER

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,7 @@ import { useToast } from '../composables/useToast';
 import router from '../router';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 10000,
 });
 
